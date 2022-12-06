3a9bd945daf74dba8548505a36467e14920c96ae
Sondre Instanes <Fyrren@gmail.com>
2022-12-06 21:01:16 +0100

    Add Basic Operation
    Added functions for addition, subtraction, multiplication and division.

---
+++ script.js
@@ -0,0 +1,19 @@
+const addition = function(a, b) {
+    return (Number(a + b));
+}
+
+const subtraction = function(a, b) {
+    return (Number(a - b));
+}
+
+const multiplication = function(a, b) {
+    return (Number(a * b));
+}
+
+const division = function(a, b) {
+    return (Number(a / b));
+}
+
+const operate = function(operator, a, b) {
+    
+}
