e9aeca03cacf0c18c937fd797d61671b48b6588e
Sondre Instanes <Fyrren@gmail.com>
2022-12-07 15:38:45 +0100

    Add QuerySelector
    Updated script to add queryselector for buttons

--- script.js
+++ script.js
@@ -1,4 +1,36 @@
-// BASIC MATH FUNCTIONS
+// ---GET BUTTONS AND DISPLAY---
+// CLEAR AND DELETE BUTTONS
+const clear = document.querySelector(".clear");
+const back = document.querySelector(".back");
+
+// NUMBER BUTTONS
+const one = document.querySelector(".one");
+const two = document.querySelector(".two");
+const three = document.querySelector(".three");
+const four = document.querySelector(".four");
+const five = document.querySelector(".five");
+const six = document.querySelector(".six");
+const seven = document.querySelector(".seven");
+const eight = document.querySelector(".eight");
+const nine = document.querySelector(".nine");
+const dot = document.querySelector(".dot");
+
+// OPERATION BUTTONS
+const plus = document.querySelector(".add");
+const minus = document.querySelector(".subtract");
+const multiply = document.querySelector(".multiply");
+const divide = document.querySelector(".divide");
+const equals = document.querySelector(".equals");
+
+// DISPLAY
+const inputText = document.querySelector(".inputtext");
+const solutionText = document.querySelector(".answertext");
+
+inputText.innerText = "";
+solutionText.innerText = "";
+
+
+// ---BASIC MATH FUNCTIONS---
 
 const addition = function(a, b) {
     return (Number(a + b));
@@ -21,3 +53,9 @@ const operate = function(operator, a, b) {
        return addition(a,b);
     }
 }
+
+// ---BUTTON FUNCTIONALITY---
+// NUMBER BUTTONS
+one.addEventListener("click", () => {
+
+});
